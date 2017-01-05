f4c9ec596cad876793e45724a4721b50fd6f133b
Edno de Moraes Fedulo <Edno.Fedulo@AD002.NET02>
2017-01-05 15:34:06 -0300

    pequenas alterações

--- app/routes.js
+++ app/routes.js
@@ -3,17 +3,10 @@ var path = require('path');
 
 module.exports = function(app){
 
-	app.get('/teste', function(req, res) {
-		console.log('second route');
-	    res.sendFile(path.join(__dirname,'../public/teste.html'));
-	});
-
-	/*app.get('*', function(req, res) {
+	app.get('*', function(req, res) {
 		console.log('default route');
-	    res.sendFile(path.join(__dirname + '/public/index.html')); // load the single view file (angular will handle the page changes on the front-end)
-	});*/
-
-	
+	    res.sendFile(path.join(__dirname, '../public/index.html'));
+	});
 
 };
 
